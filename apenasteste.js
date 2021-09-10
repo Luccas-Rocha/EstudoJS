071ed601d674801499bed2a57227af226fa7c62a
unknown <rochaluccas029@gmail.com>
2021-09-10 15:26:12 -0300

    apenas para ter o icone de JS

---
+++ apenasteste.js
@@ -0,0 +1,9 @@
+const altura = 1.84;
+const peso = 85;
+const imc = peso/(altura*altura);
+const idade = 20;
+const nome = 'who the fuck';
+
+console.log(`${nome} tem ${idade} anos, 
+pesa ${peso}kg, tem ${altura} de altura 
+e seu imc é de ${imc}`);
