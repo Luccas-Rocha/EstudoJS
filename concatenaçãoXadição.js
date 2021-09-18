ad4c3871ecd40f1397154749dca71db36b520c36
unknown <rochaluccas029@gmail.com>
2021-09-18 16:05:13 -0300

    exercicio novo

---
+++ concatenaçãoXadição.js
@@ -0,0 +1,6 @@
+let num1 = prompt("digite um numero");
+let num2 = prompt("digite outro numero");
+num2 = parseInt(num2);
+num1 = parseInt(num1);
+let resultado = num1 + num2;
+alert("o resultado da soma de "+num1+ " + " +num2+ ' é de '+resultado);
