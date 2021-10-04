3c7e964e22f1d52ddfcf6a0ea4430ba5bea7ff89
unknown <rochaluccas029@gmail.com>
2021-10-04 11:16:31 -0300

    estudo sobre uso de if, else if e else

---
+++ ifElseifElse.js
@@ -0,0 +1,35 @@
+const hora = 10;
+
+if (hora >= 12 && hora <= 18){
+    console.log("Boa tarde")
+}
+
+else if(hora < 12 && hora >= 0){
+    console.log("Bom dia")
+}
+
+else if(hora >= 19 && hora <=23){ 
+    console.log("boa noite")
+}
+
+else{
+    console.log("olá")
+}
+ 
+
+
+
+const numero = 10 
+
+if (numero >= 0 &&numero <=5){
+    console.log('o numero está entre 0 e 5')
+}
+else if(numero >= 6 && numero <=8){
+    console.log('o numero está entre 6 e 8')
+}
+else if (numero >= 9 && numero <=11){
+    console.log('o numero está entre 9 e 11')
+}
+else{
+    console.log('o numero não está entre 0 e 11')
+}
